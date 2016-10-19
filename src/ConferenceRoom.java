e3719a9e4fb19fb0efcab20d7058a45ab328b366
Matthew Mansor <matthewmansor@gmail.com>
2016-10-19 10:47:12 -0400

    Added stubs for Conference Room

---
+++ src/ConferenceRoom.java
@@ -0,0 +1,35 @@
+
+
+public class ConferenceRoom {
+
+  private TeamLead owner;
+  private int waitingFor;
+
+  public ConferenceRoom() {
+    owner = null;
+    waitingFor = 0;
+  }
+
+
+  /**
+  * Lets the team lead reserve the conference room.
+  * The team lead reserves the room for his meeting with his reference and the
+  * number of team members he's waiting for. He and all future members that
+  * arrive wait for everyone to be there before starting the meeting.
+  */
+  public synchronized void reserveRoom(TeamLead lead, int members) {
+    owner = lead;
+    waitingFor = members;
+  }
+
+
+  /**
+  * Lets a team member arrive at the conference room.
+  * A team member arrives at the conference room and asks if it's currently
+  * reserved by their team lead. If it is, they stay for the meeting and arrive
+  * returns true, otherwise it returns false and returns immediately.
+  */
+  public synchronized boolean arrive(TeamLead lookingFor) {
+    return false;
+  }
+}
